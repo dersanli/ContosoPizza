35b5025405fe398d25eb830122c285e036f2ec95
Devrim <devrim.ersanli@fly.virgin.com>
2023-06-23 16:01:49 +0100

    Initial commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoPizza.Controllers;
+
+[ApiController]          // [ApiController] enables opinionated behaviors that make it easier to build web APIs.
+[Route("[controller]")]
+// [Route] defines the routing pattern [controller].
+// The [controller] token is replaced by the controller's name (case-insensitive, without the Controller suffix).
+// This controller handles requests to https://localhost:{PORT}/weatherforecast.
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IEnumerable<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+    }
+}
